27288e60ce00c9d2fde516047dfadd222a43fccc
nogto7 <nogto8888@gmail.com>
2026-01-02 15:15:03 +0800

    login redirect

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('news', absolute: false));
+        return redirect()->intended(route('admin/news/list', absolute: false));
     }
 
     /**
